e7d3d6b6850f84ada33e1ffeb061755f141e0673
Jack <jack.w.barker@durham.ac.uk>
2020-06-24 13:55:16 +0100

    added files to repo

---
+++ config.py
@@ -0,0 +1,28 @@
+import argparse
+from torchvision import transforms
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--size', type=int, default=256)
+parser.add_argument('--epoch', type=int, default=560)
+parser.add_argument('--lr', type=float, default=3e-4)
+parser.add_argument('--sched', type=str)
+parser.add_argument('path', type=str)
+
+args = parser.parse_args()
+
+transform = transforms.Compose(
+        [
+            transforms.Resize(args.size),
+            transforms.CenterCrop(args.size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        ]
+    )
+
+
+def get_args():
+    return args
+
+def get_transform():
+    return transform
---
+++ main.py
@@ -0,0 +1,10 @@
+import config
+from torchvision import datasets
+from torch.utils.data import DataLoader
+
+args = config.get_args()
+transform = config.get_transform()
+
+dataset = datasets.ImageFolder(args.path, transform=transform)
+loader = DataLoader(dataset, batch_size=26, shuffle=True, num_workers=0)
+
