a1f8f9e3e21a772f38a9224f87858146f1748666
Jack <jack.w.barker@durham.ac.uk>
2020-06-24 14:03:24 +0100

    tidied up code a bit

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ transform = config.get_transform()
 dataset = datasets.ImageFolder(args.path, transform=transform)
 loader = DataLoader(dataset, batch_size=26, shuffle=True, num_workers=0)
 
+for i, (img, label) in enumerate(loader) :
+    print(img)
+
